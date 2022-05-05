b1d642eb08eb27f476d7f78c7e1601f7bf9c79c0
root <root@ip-172-31-20-110.ec2.internal.com>
2022-05-05 09:53:15 +0000

    message1

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+total=`ls /root/ | wc -l`
+echo "Total = $total"
+old_files=`expr $total - 50`
+echo "Old files = $old_files"
+if [ $old_files -gt 0 ]
+then 
+	ls -rt | head -$old_files | xargs rm -rf
+fi
+
